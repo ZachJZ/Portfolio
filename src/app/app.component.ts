97ce0fea36ea687d614c312d1bbdd174bc031622
ZachZ <ZachZ@users.noreply.github.com>
2024-12-24 23:27:14 -0500

    Commit for the day

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { SideBarComponent } from "./side-bar/side-bar.component";
+import { HeaderComponent } from "./header/header.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, SideBarComponent],
+  imports: [CommonModule, RouterOutlet, SideBarComponent, HeaderComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
